ff0ea58432e4a11b7f9ad155a59126144af376e3
Cyrus Harmon <ch-lisp@bobobeach.com>
2008-04-19 14:20:57 -0700

    use (dirname "$0") so that running make-dist.sh from another directory works

--- make-dist.sh
+++ make-dist.sh
@@ -3,7 +3,7 @@ SYSTEMS=":${PACKAGE} :${PACKAGE}-test"
 
 sbcl --noinform --noprint \
     --eval '(require :asdf)' \
-    --eval "(pushnew (make-pathname :directory \""`pwd`"\") asdf:*central-registry*)" \
+    --eval "(pushnew (make-pathname :directory \""`pwd`/`(dirname "$0")`"\") asdf:*central-registry*)" \
     --eval "(asdf:operate 'asdf:load-op 'ch-util)" \
     --eval "(asdf:operate 'asdf:load-op 'asdf-package)" \
     --eval "(asdf-package:package-system ${SYSTEMS})" \
